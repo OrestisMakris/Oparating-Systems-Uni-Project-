9328051604dca49cd334b80d9e1245ff2d7dbec6
Orestis Makris <79021400+OrestisMakris@users.noreply.github.com>
2023-01-20 22:45:19 +0200

    f

--- Part I/ask2.c
+++ Part I/ask2.c
@@ -1,4 +1,7 @@
-// msgq1.c
+// Zikos Spyridon 1084581
+// Kyriakoulopoulos Kallinikos 1084583
+// Makris Orestis-Antonis 1084516
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -16,9 +19,11 @@ double get_wtime(void) {
     gettimeofday(&t, NULL);
     return (double)t.tv_sec + (double)t.tv_usec*1.0e-6;
 }
+
 double f(double x) {
     return log(x)*sqrt(x);
 }
+
 double get_integral(double a, double b, int n) {
     const double dx = (b-a)/n;
     double S = 0;
@@ -31,70 +36,55 @@ double get_integral(double a, double b, int n) {
     return S;
 }
 
-void server(double S)
-{  
-	key_t key;
-	int msgid;
-
-	key = ftok("ask2.c", 61235);
-	msgid = msgget(key, 0666 | IPC_CREAT);
+void server(double S) {  
+	int key = ftok("ask2.c", 61235);
+	int msgid = msgget(key, 0666 | IPC_CREAT);
 
 	msgsnd(msgid, &S, sizeof(double), 0);
-
     exit(1);
 }
 
-double client()
-{
-    key_t key;
-    int msgid;
-
-    key = ftok("ask2.c", 61235);
-    msgid = msgget(key, 0666 /*| IPC_CREAT*/);
+double client() {
+    int key = ftok("ask2.c", 61235);
+    int msgid = msgget(key, 0666);
     
     double k;
     msgrcv(msgid, &k, sizeof(double), 0, 0);
-    
     return k;
 }
 
-int main(int argc, char *argv[])
-{
-  double a = 1.0;
-  double b = 4.0;
+int main(int argc, char *argv[]) {
+    double a = 1.0;
+    double b = 4.0;
 
-  unsigned long const n = 1e8;
-  int procedures = atoi(argv[1]);
-  long int size_of_procedure = ceil(n / procedures);
-  
+    unsigned long const n = 1e9;
+    int procedures = atoi(argv[1]);
+    long int size_of_procedure = ceil(n / procedures);
 
-  int pid;
-  double total = 0;
-  double t0 = get_wtime();
-  
-  for(int i=0; i<procedures; i++)
-  {
-    pid = fork();
-    if (pid == 0)
-    {
-      double S = get_integral(a+((b-a)/procedures)*i, a+((b-a)/procedures)*(i+1), size_of_procedure);
-      server(S);
+    int pid;
+    double total = 0;
+    double t0 = get_wtime();
+
+    for(int i=0; i<procedures; i++) {
+        pid = fork();
+        if (pid == 0) {
+            double S = get_integral(a+((b-a)/procedures)*i, a+((b-a)/procedures)*(i+1), size_of_procedure);
+            server(S);
+        }
     }
-  }
-  
-  for(int i=0; i<procedures; i++) {
-    if (pid > 0) {
-        waitpid(-1, NULL, 0);
-        total += client();
-    }
-  }
-  
-  int key = ftok("ask2.c", 61235);
-  int msgid = msgget(key, 0666 /*| IPC_CREAT*/);
-  msgctl(msgid, IPC_RMID, NULL);
-  
-  double t1 = get_wtime();
 
-  printf("Time=%lf seconds, Result=%.8f\n", t1-t0, total);
-  return 0;
+    for(int i=0; i<procedures; i++) 
+        if (pid > 0) {
+            waitpid(-1, NULL, 0);
+            total += client();
+        }
+
+    int key = ftok("ask2.c", 61235);
+    int msgid = msgget(key, 0666);
+    msgctl(msgid, IPC_RMID, NULL);
+
+    double t1 = get_wtime();
+
+    printf("Time=%lf seconds, Result=%.8f\n", t1-t0, total);
+    return 0;
 }
